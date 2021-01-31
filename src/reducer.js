bbf2fbd0dc30b4ef9ed0726b7d6001866fcf5f2e
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-02-01 04:18:44 +0900

    feat: finish part 3

---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
+
+export default function rootReducer(state = {}, action) {
+  // always return a new object for the root state
+  return {
+    // the value of `state.todos` is whatever the todos reducer returns
+    todos: todosReducer(state.todos, action),
+    // For both reducers, we only pass in their slice of the state
+    filters: filtersReducer(state.filters, action),
+  }
+}
