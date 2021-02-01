f2454e3a977362b7c5844bf72000bfc12ba21812
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-02-01 23:46:05 +0900

    feat: finish part 4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App'
 
 import './api/server'
+import store from './store'
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
 
 ReactDOM.render(
   <React.StrictMode>
